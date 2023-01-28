b51ad003bda40298db92cc486a75fe604215d482
Jie Wang（王杰） <wangjie212@mails.ucas.ac.cn>
2023-01-28 14:25:49 +0800

    add solution example

--- example.m
+++ example.m
@@ -10,3 +10,15 @@ g = [x(1)*x(2)];
 h = [x(1)^2+x(2)^2-1];
 [blk, At, C, b] = sosprogram(a, f, g, h, x, 2);
 sqlp(blk, At, C, b);
+
+% Extract solutions
+yalmippath = '../../../Programs/YALMIP';
+addpath(genpath(yalmippath));
+
+sdpvar x1 x2
+obj = x1^4-x1^2+x2^2-2*x1*x2+1;
+[info,sol,data] = solvemoment([],obj,[],3);
+
+
+nsol = extract_solution(data.moment{3}, data.x, data.monomials);
+
